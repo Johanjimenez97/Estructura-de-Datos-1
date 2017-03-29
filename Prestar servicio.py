53f087e7d3891b1df9e5c9a5d384fdd1fd73088e
Johanjimenez97 <Johanjimenez970@gmail.com>
2017-03-29 18:43:48 -0500

    Add files via upload

---
+++ Prestar servicio.py
@@ -0,0 +1,21 @@
+mas = 1
+edad = 18
+prot = 0
+est = 0
+col = 1
+if mas == 1:
+    if edad >= 18:
+        if prot == 0:
+            if est == 0:
+                if col == 1:
+                    print "Prestar servicio"
+                else:
+                    print "no presta servicio"
+            else:
+                print "no presta servicio"
+        else:
+            print "no presta servicio"
+    else:
+        print "no presta servicio"
+else:
+    print "no presta servicio"
